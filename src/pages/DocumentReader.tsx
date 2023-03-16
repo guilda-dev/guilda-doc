7b943c5df22e3b7bc146a8787d85977a9c873b4b
Bill Stark <billstark001@gmail.com>
2023-03-17 08:41:23 +0900

    replace main page

--- src/pages/DocumentReader.tsx
+++ src/pages/DocumentReader.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import NavTree from '@/components/nav/NavTree';
-import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import MarkdownDisplay from '@/components/markdown/MarkdownDisplay';
 import { LeftSideFrame } from '@/components/page/PageFrame';
 import { SiteNavTree } from '@/common/config';
-import { getStaticResource, ResourceMeta } from '@/common/io';
-import path from 'path-browserify';
-import { useGlobalSetting } from '@/components/common/GlobalSetting';
-
-const DOC_PREFIX = '/md';
+import { useResourceStore } from '@/components/markdown/node/ResourceNode';
 
 
 const ErrorPage = (props: { error?: Error }) => {
@@ -20,35 +15,20 @@ const ErrorPage = (props: { error?: Error }) => {
 };
 
 const DocumentReader = () => {
-  const [resource, setResource] = useState<string | Error | undefined>(undefined);
-  const [, setMeta] = useState<ResourceMeta | undefined>(undefined);
 
   const location = useLocation();
-  const id = location.pathname;
-  const setting = useGlobalSetting();
+  const [resource, error] = useResourceStore({
+    link: location.pathname, 
+    descriptor: { format: ['md', 'html'] },
+  });
 
-  useEffect(() => {
-    getStaticResource(
-      id,
-      { format: ['md', 'html'] },
-      setting.language
-    ).then(([data, err, meta]) => {
-      if (err)
-        setResource(err);
-      else {
-        setResource(data);
-      }
-      setMeta(meta);
-    }).catch((err => setResource(err)));
-    
-  }, [id, setting.language]);
   return <>
     <LeftSideFrame>
       <NavTree rootNode={SiteNavTree} />
     </LeftSideFrame>
     { resource === undefined && <div>loading...</div> }
     { typeof resource === 'string' && <MarkdownDisplay>{ resource }</MarkdownDisplay> }
-    { resource instanceof Error && <ErrorPage error={resource} /> }
+    { error instanceof Error && <ErrorPage error={error} /> }
   </>;
 };
 
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -5,71 +5,21 @@ import NavTree from '../components/nav/NavTree';
 import { useGlobalSetting } from '../components/common/GlobalSetting';
 import { LeftSideFrame } from '../components/page/PageFrame';
 import { SiteNavTree } from '../common/config';
+import MarkdownDisplay from '@/components/markdown/MarkdownDisplay';
+import { useResourceStore } from '@/components/markdown/node/ResourceNode';
 
 const MainPage = () => {
-  const [count, setCount] = useState(0);
-  const { darkMode, setSetting: setStyle } = useGlobalSetting();
+  
+  const [resource] = useResourceStore({
+    link: '/index', 
+    descriptor: { format: ['md', 'html'] }
+  });
 
   return <>
     <LeftSideFrame>
       <NavTree rootNode={SiteNavTree} />
     </LeftSideFrame><div className="App">
-      <h1>This is a random temporary main page</h1>
-      <div className="card">
-        <button onClick={() => {
-          setCount((count) => count + 1);
-          setStyle({ darkMode: !darkMode });
-        }}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-          Edit <code>src/App.tsx</code> and save to test HMR<br></br>
-        </p>
-      </div>
+      <MarkdownDisplay>{ resource }</MarkdownDisplay>
     </div></>;
 };
 
